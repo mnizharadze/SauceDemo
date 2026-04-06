9bd9d5679ff77b286b3585dc12d83f127c53451f
mnizharadze <m.nizharadze@extra.ge>
2026-04-06 19:45:58 +0400

    Add login tests for valid and invalid scenarios

---
+++ QA-library/Tests/login.spec.js
@@ -0,0 +1,21 @@
+const { test } = require('@playwright/test');
+const { loginAsStandardUser, loginWithCredentials, loginAsLockedUser } = require('../qa-library/actions/login');
+const { validateLoginSuccess, validateLoginError } = require('../qa-library/validations/login');
+
+test('TC01 - Valid login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await loginAsStandardUser(page);
+  await validateLoginSuccess(page);
+});
+
+test('TC02 - Invalid password', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await loginWithCredentials(page, 'standard', 'wrong_password');
+  await validateLoginError(page, 'Username and password do not match');
+});
+
+test('TC03 - Locked user login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await loginAsLockedUser(page);
+  await validateLoginError(page, 'Sorry, this user has been locked out.');
+});
